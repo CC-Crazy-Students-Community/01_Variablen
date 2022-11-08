8961810c25be70df22acbcbb58d42d73fb17613e
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-08 12:15:22 +0100

    kombination von var und string

--- script_01.js
+++ script_01.js
@@ -16,11 +16,13 @@
 /***** 02 Deklaration + Wertzuweisung I *****/
 let firstName;                              // Deklaration (Definition)
 firstName = "Max";                          // Wertzuweisung
-console.log(firstName);                     // Ausgabe
+console.log(firstName);                     // Ausgabe (Max)
 
 let lastName = "Mustermann";                // Deklaration (Definition)
-console.log(lastName);                      // Ausgabe
+console.log(lastName);                      // Ausgabe (Mustermann)
 
 
 /***** 03 Kombination von Variablen mit + und Leerzeichen in Stringform *****/
-console.log(firstName + " " + lastName);    // Ausgabe
+console.log(firstName + " " + lastName);    // Ausgabe (Max Mustermann)
+
+
