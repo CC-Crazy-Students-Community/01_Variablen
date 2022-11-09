ef7b6e698b84e271952a6262376aeaa371fc9904
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-09 09:14:37 +0100

    03-a Konstanten

--- script_01.js
+++ script_01.js
@@ -7,6 +7,7 @@
  *  Block-Kommentar
  * 
 */
+// einfaches Kommentar
 
 // console.log("Hallo");
 // console.log(firstName);
@@ -15,27 +16,32 @@
 /***** 02 Deklaration + Wertzuweisung I *****/
 // let firstName;                                              // Deklaration (Definition)
 // firstName = "Max";                                          // Wertzuweisung
-// console.log(firstName);                                     // Ausgabe (Max)
+// console.log( firstName );                                   // Ausgabe (Max)
 
 // let lastName = "Mustermann";                                // Deklaration und Wertzuweisung
-// console.log(lastName);                                      // Ausgabe (Mustermann)
+// console.log( lastName );                                    // Ausgabe (Mustermann)
 
 
-/***** 03 Kombination von Variablen mit + und Leerzeichen in Stringform *****/
-// console.log(firstName + " " + lastName);                    // Ausgabe (Max Mustermann)
+/***** 02-a Kombination von Variablen mit + und Leerzeichen in Stringform *****/
+// console.log( firstName + " " + lastName );                  // Ausgabe (Max Mustermann)
 
 
-/***** 04 Deklaration + Wertzuweisung II *****/
+/***** 03 Deklaration + Wertzuweisung II *****/
 // let firstName, lastName;                                    // Deklaration (Definition) mehrerer Variablen gleichzeitig
-// firstName = prompt("Wie ist dein Vorname?: ");              // Wertzuweisung per Usereingabe
-// lastName = prompt("Wie ist dein Nachname?: ");              // Wertzuweisung
-// console.log("Danke, " + firstName + " " + lastName);        // Ausgabe (Max Mustermann)
+// firstName = prompt( "Wie ist dein Vorname?: " );            // Wertzuweisung per Usereingabe
+// lastName = prompt( "Wie ist dein Nachname?: " );            // Wertzuweisung
+// console.log( "Danke, " + firstName + " " + lastName );      // Ausgabe (Max Mustermann)
 
 
 /* Javascript ist eine untypisierte Sprache  |  untyped */
-let test;                                                   // Deklaration (Definition)
-test = "Hi";                                                // Deklaration eines Strings
-test = 2;                                                   // Deklaration eines Integerwertes
-test = true;                                                // Deklaration eines Boolschen Wert
+// let test;                                                   // Deklaration (Definition)
+// test = "Hi";                                                // Deklaration eines Strings
+// test = 2;                                                   // Deklaration eines Integerwertes
+// test = true;                                                // Deklaration eines Boolschen Wert
 
-console.log(test);                                          // Ausgabe
+// console.log( test );                                        // Ausgabe (true)
+// console.log( "Datentyp: " + typeof test );                  // Ausgabe des Datentyps (Datentyp: boolean)
+
+/***** 03-a Konstanten *****/
+
+
