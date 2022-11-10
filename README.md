597ee31b904b11041b0b21785ff57fdf4cd76ebc
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-10 06:03:43 +0100

    changed formatting and texterrors

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# 01_Variablen
+# Variablen
 
 ### Variablen | Konstanten
-
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
 - https://www.w3schools.com/js/js_let.asp
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
-
 
 ### Zuordnung | Assignment
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Assignment
@@ -19,3 +17,16 @@
 - https://www.w3schools.com/jsref/jsref_obj_boolean.asp
 - https://www.w3schools.com/jsref/jsref_obj_number.asp
 - https://developer.mozilla.org/en-US/docs/Glossary/undefined
+
+
+# CodeChallenge Variablen
+
+## Die Code Challange mit Beispielen
+
+* Geben Sie über eine Prompt-Anforderung eine Zahl ein.
+* Geben Sie über eine Prompt-Anforderung eine zweite Zahl ein.
+* Berechnen Sie die Summe der beiden Zahlen.
+* Geben Sie die Summe der Zahlen in die Konsole aus: "Die Summe der Zahlen ist: summe"
+
+
+###### sandrosimperl.cc@gmail.com
--- script_01.js
+++ script_01.js
@@ -1,7 +1,8 @@
 "use strict";
 
-/***** Variablen 01 ******/
-/* Sabine ist voll TOLL */
+/*****************************************************/
+/*********             VARIABLEN             *********/
+/*****************************************************/
 
 /** 
  * 
@@ -10,51 +11,56 @@
 */
 // einfaches Kommentar
 
-// console.log("Hallo");
-// console.log(firstName);
+    console.log("Hallo");
+    console.log(firstName);                                      // Fehler, weil Variable noch nicht deklariert ist
+
+/*****************************************************/
+/*********   Deklaration + Wertzuweisung I   *********/
+/*****************************************************/
+    let firstName1;                                            // Deklaration (Definition)
+    firstName1 = "Max";                                        // Wertzuweisung
+    console.log( firstName1 );                                 // Ausgabe (Max)
+
+    let lastName1 = "Mustermann";                              // Deklaration und Wertzuweisung
+    console.log( lastName1 );                                  // Ausgabe (Mustermann)
+
+/*****************************************************/
+/***********   Kombination von Variablen   ***********/
+/***********   und Strings mit + vereint   ***********/
+/*****************************************************/
+    console.log( firstName1 + " " + lastName1 );                // Ausgabe (Max Mustermann)
 
 
-/***** 02 Deklaration + Wertzuweisung I *****/
-// let firstName;                                              // Deklaration (Definition)
-// firstName = "Max";                                          // Wertzuweisung
-// console.log( firstName );                                   // Ausgabe (Max)
-
-// let lastName = "Mustermann";                                // Deklaration und Wertzuweisung
-// console.log( lastName );                                    // Ausgabe (Mustermann)
-
-
-/***** 02-a Kombination von Variablen mit + und Leerzeichen in Stringform *****/
-// console.log( firstName + " " + lastName );                  // Ausgabe (Max Mustermann)
-
-
-/***** 03 Deklaration + Wertzuweisung II *****/
-// let firstName, lastName;                                    // Deklaration (Definition) mehrerer Variablen gleichzeitig
-// firstName = prompt( "Wie ist dein Vorname?: " );            // Wertzuweisung per Usereingabe
-// lastName = prompt( "Wie ist dein Nachname?: " );            // Wertzuweisung
-// console.log( "Danke, " + firstName + " " + lastName );      // Ausgabe (Max Mustermann)
-
+/******************************************************/
+/*********   Deklaration + Wertzuweisung II   *********/
+/******************************************************/
+    let firstName2, lastName2;                                  // Deklaration (Definition) mehrerer Variablen gleichzeitig
+    firstName2 = prompt( "Wie ist dein Vorname?: " );           // Wertzuweisung per Usereingabe
+    lastName2 = prompt( "Wie ist dein Nachname?: " );           // Wertzuweisung
+    console.log( "Danke, " + firstName2 + " " + lastName2 );    // Ausgabe (Max Mustermann)
 
 /* Javascript ist eine untypisierte Sprache  |  untyped */
-// let test;                                                   // Deklaration (Definition)
-// test = "Hi";                                                // überschreibe Deklaration mit String
-// test = 2;                                                   // überschreibe Deklaration mit Integerwert
-// test = true;                                                // überschreibe Deklaration mit Boolschem Wert
+    let test;                                                   // Deklaration (Definition)
+    test = "Hi";                                                // überschreibe Deklaration mit String
+    test = 2;                                                   // überschreibe Deklaration mit Integerwert
+    test = true;                                                // überschreibe Deklaration mit Boolschem Wert
 
-// console.log( test );                                        // Ausgabe (true)
-// console.log( "Datentyp: " + typeof test );                  // Ausgabe des Datentyps (Datentyp: boolean)
+    console.log( test );                                        // Ausgabe (true)
+    console.log( "Datentyp: " + typeof test );                  // Ausgabe des Datentyps (Datentyp: boolean)
 
-/***** 03-a Konstanten *****/
-
+/******************************************************/
+/*********             Konstanten             *********/
+/******************************************************/
 /* Falsche Deklaration einer Konstante  |  Kann nicht verändert werden, da es Konstant sein muss */
-// const test = "Hi";                                          // Deklaration Konstante
-// test = "Hallo";                                             // überschreibe Konstante (ergibt Fehler)
-// console.log( test );                                        // Ausgabe (Uncaught TypeError: Assignment to constant variable.)
+    const test1 = "Hi";                                         // Deklaration Konstante
+    test1 = "Hallo";                                            // überschreibe Konstante (ergibt Fehler)
+    console.log( test1 );                                       // Ausgabe (Uncaught TypeError: Assignment to constant variable.)
 
 /* Falsche Deklaration einer Konstante  |  Kann nicht nachträglich geändert werden, muss also sofort zugewiesen werden */
-// const test;                                                 // Deklaration Konstante
-// test = "Hi";                                                // Wertzuweisung Konstante (ergibt Fehler)
-// console.log( test );                                        // Ausgabe (Uncaught SyntaxError: Missing initializer in const declaration)
+    const test2;                                                // Deklaration Konstante
+    test2 = "Hi";                                               // Wertzuweisung Konstante (ergibt Fehler)
+    console.log( test2 );                                       // Ausgabe (Uncaught SyntaxError: Missing initializer in const declaration)
 
 /* Richtige Deklaration einer Konstante */
-const test = "Hi";                                          // Deklaration Konstante
-console.log( test );                                        // Ausgabe (Hi)
+    const test3 = "Hi";                                          // Deklaration Konstante
+    console.log( test3 );                                        // Ausgabe (Hi)
